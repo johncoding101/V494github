78a53886165d594a6f53971fc1667d0b964c11cd
johncoding101 <johncoding101@gmail.com>
2025-06-22 21:50:27 -0400

    update func1.py func2.py

---
+++ func1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Sunday Jun 22 15:44:10 2025
+Udemy -100 Days of code (Angela Yu)
+video -494  Git and the command line
+Author: JohnC
+'''
+
+# *   GitHub
+
+
+def func1_printer():
+    """_summary_
+    """
+    print("function 1 ")
---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Sunday Jun 22 15:44:10 2025
+Udemy -100 Days of code (Angela Yu)
+video -494  Git and the command line
+Author: JohnC
+'''
+
+# *   GitHub
+
+print("main program")
